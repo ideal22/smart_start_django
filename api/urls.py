5c23f322eccbf787dfe8ea19fbeb0bc4345df631
dadakhon09 <odadaxon27@mail.ru>
2019-07-19 22:59:09 +0500

    wtf

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [
+	path('api/token',TokenObtainPairView.as_view()),
+	path('api/token/refresh', TokenRefreshView.as_view()),
+]
---
+++ startups/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from users.models import Users
+
+
+class Notifications(models.Model):
+	user = models.ForeignKey(Users,on_delete=models.CASCADE)
+	text = models.TextField()
+	link = models.CharField(max_length=255)
+
+class Comments(models.Model):
+	user = models.ForeignKey(Users, on_delete=models.CASCADE)
+	startup = models.ForeignKey('Startups', on_delete=models.CASCADE)
+	text = models.TextField()
+
+class Startups(models.Model):
+	title = models.CharField(max_length=255)
+	description = models.TextField()
+	user = models.ForeignKey(Users, on_delete=models.CASCADE)
+	startupStatus = models.ForeignKey(StartupStatuses, on_delete=models.CASCADE)
+
+class StartupStatuses(models.Model):
+	status = models.CharField(max_length=255)
+
+class StartupFiles(models.Model):
+	fileType = models.CharField(max_length=255)
+	
