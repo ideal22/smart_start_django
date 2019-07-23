691dacaea5c1e5c842bc01c8c12844b73467031b
dadakhon09 <odadaxon27@mail.ru>
2019-07-23 13:57:16 +0500

    asd

--- SmartStart/settings.py
+++ SmartStart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o%v+6h$mx8_zt9j)#@h-5z-ip4c9miz&w0yq9k7-p#k6_dagdw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
