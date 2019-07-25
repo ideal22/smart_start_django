fc773e80eb878a159113bb691c7480da31b0127a
ideal22 <eshonkulov_1995@mail.ru>
2019-07-25 13:44:25 +0300

    wtf

--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from api.views import StartupsAPIView
-
-urlpatterns = [
-	path('startups',StartupsAPIView.as_view(), name='api-startups'),
-]
--- users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from users.models import Users, UserProfiles
-
-# Register your models here.
-admin.site.register(Users)
-admin.site.register(UserProfiles)
